005538c623dbcba09aac0ef0a9d647a406ac0f35
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-08-02 14:36:44 +0200

    * refactoring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from math import sqrt
 from data_manager import DataManager
 import random
@@ -10,63 +8,56 @@ import utils
 # RETRIEVE SYMBOLS
 print('0) Retrieving SP500 symbols..')
 
-stocks = utils.sp500_symbols()
+sp500_symbols = utils.sp500_symbols()
+sp500_symbols = random.sample(sp500_symbols, 10)
 
-# TODO: remove sampling
-stocks = random.sample(stocks, 10)
+split_pt = int(len(sp500_symbols) * .8)
+train_symbols = sp500_symbols[:split_pt]
+test_symbols = sp500_symbols[split_pt:]
 
-split_pt = int(len(stocks)*.8)
-train_stocks = stocks[:split_pt]
-test_stocks = stocks[split_pt:]
+assert len(train_symbols) > 0 and len(test_symbols), 'no valid symbols found'
 
-print('Found: {} train, {} test'.format(len(train_stocks), len(test_stocks)))
+print('Found: {} train symbols - {} test symbols'.format(len(train_symbols),
+                                                         len(test_symbols)))
 
 # BUILD TIME WINDOWS
-print('1) Building time windows..')
+print('1) Splitting into time windows..')
 
 timestep = 144
 future_window = 30
 features = ['open', 'high', 'low']
 data_manager = DataManager(timestep, future_window, features, debug=False)
-X_train, y_train = data_manager.build_windows(train_stocks)
-X_test, y_test = data_manager.build_windows(test_stocks)
+X_train, y_train = data_manager.build_windows(train_symbols)
+X_test, y_test = data_manager.build_windows(test_symbols)
 
-assert (len(X_train) > 0) and (len(X_test) > 0), 'insufficient number of samples'
+assert len(X_train) > 0 and len(X_test) > 0, 'insufficient number of samples'
 
-print('Built: {} train, {} test'.format(len(X_train), len(X_test)))
+print('Built: {} train time windows - {} test time windows'.format(len(X_train),
+                                                                   len(X_test)))
 
 # BALANCE DATASET
-print('2) Balancing train data..')
+print('2) Balancing data..')
 
-downtrend_win_count = len(y_train) - np.count_nonzero(np.array(y_train))
-uptrend_win_idx, = np.where(y_train)
+X_train, y_train = utils.balance(X_train, y_train)
+X_test, y_test = utils.balance(X_test, y_test)
 
-print('Before: {} downtrend, {} uptrend'.format(downtrend_win_count, len(uptrend_win_idx)))
+assert len(X_train) == len(y_train) and len(X_test) == len(y_test), 'non matching samples and targets lengths'
+assert len(X_train) > 0 and len(X_test), 'insufficient number of samples'
 
-np.random.shuffle(uptrend_win_idx)
-X_train = np.delete(X_train, uptrend_win_idx[downtrend_win_count:], axis=0)
-y_train = np.delete(y_train, uptrend_win_idx[downtrend_win_count:])
+print('Train: {} downtrend time windows - {} uptrend time windows'.format(len(np.where(y_train == 0)[0]),
+                                                                          len(np.where(y_train)[0])))
 
-assert len(X_train) == len(y_train), 'non matching samples and targets lengths'
-assert len(X_train) > 0, 'insufficient number of samples'
-
-print('After: {} downtrend, {} uptrend'.format(len(np.where(y_train == 0)[0]), len(np.where(y_train)[0])))
+print('Test: {} downtrend time windows - {} uptrend time windows'.format(len(np.where(y_test == 0)[0]),
+                                                                         len(np.where(y_test)[0])))
 
 # TRAIN MODEL
 print('3) Training model..')
 
 ssize = int(sqrt(timestep))
-chns = len(features)
-print('Timestep: {0} - Futurestep: {1} - Input size: {2}x{2}x{3}'.format(timestep, future_window, ssize, chns))
+input_size = (ssize, ssize, len(features))
+print('Timestep: {} - Futurestep: {} - Input size: {}'.format(timestep, future_window, input_size))
 
-model = Sequential()
-model.add(Conv2D(20, (3, 3), padding="same", input_shape=(ssize, ssize, chns)))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(20, (3, 3), padding="same"))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Dense(units=1, activation='sigmoid'))
+model = utils.cnn(input_size)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(X_train, y_train, shuffle=True, epochs=10, validation_split=0.2)
 
--- utils.py
+++ utils.py
@@ -1,10 +1,38 @@
 from datapackage import Package
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 
 
 def sp500_symbols():
+    sp500 = []
     package = Package('https://datahub.io/core/s-and-p-500-companies/datapackage.json')
     for resource in package.resources:
         if resource.descriptor['datahub']['type'] == 'derived/csv':
             sp500 = [s[0].encode('utf-8') for s in resource.read()]
 
-    assert len(sp500) > 0, 'no valid symbols found'
+    return sp500
+
+
+def balance(x, y):
+    false_y_count = len(y) - np.count_nonzero(np.array(y))
+    true_y_idx, = np.where(y)
+
+    np.random.shuffle(true_y_idx)
+    x = np.delete(x, true_y_idx[false_y_count:], axis=0)
+    y = np.delete(y, true_y_idx[false_y_count:])
+
+    return x, y
+
+
+def cnn(input_size):
+    model = Sequential()
+    model.add(Conv2D(20, (3, 3), padding="same", input_shape=(input_size[0], input_size[1], input_size[2])))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(20, (3, 3), padding="same"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    return model
