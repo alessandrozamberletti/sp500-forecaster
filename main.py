d6f85df9e6f3b296ba5ea0e4022a7771a48484cf
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-08-30 13:56:20 +0200

    * added debug arg * added out dir creation

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 from symbol_manager import SymbolManager
 from sp500 import SP500
 import utils
+from argparse import ArgumentParser
+import os
+
 
 timestep = 144
 futurestep = 30
-debug = False
 features = ['high', 'low', 'close']
+out_dir = 'out'
+
+parser = ArgumentParser(description='Predict future stock trend.')
+parser.add_argument('--debug', action='store_true', default=False, help='show visual information')
+args = parser.parse_args()
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 # RETRIEVE SYMBOLS
 print('* Retrieving S&P500 data..')
@@ -16,7 +25,7 @@ print('** {} train symbols - {} test symbols'.format(len(train_symbols), len(tes
 
 # BUILD TIME WINDOWS
 print('* Computing time windows..')
-train_data = SymbolManager(train_symbols, features, timestep, futurestep, debug=debug).balance()
+train_data = SymbolManager(train_symbols, features, timestep, futurestep, debug=args.debug).balance()
 assert len(train_data.x) == len(train_data.y) and len(train_data.x) > 0, 'insufficient number of samples'
 print('** {} ↓time windows - {} ↑time windows'.format(utils.count_neg(train_data.y), utils.count_pos(train_data.y)))
 
@@ -24,16 +33,18 @@ print('** {} ↓time windows - {} ↑time windows'.format(utils.count_neg(train_
 print('* Training model..')
 print('** timestep: {} - futurestep: {}'.format(timestep, futurestep))
 model, hist = utils.build_and_train_cnn(train_data, epochs=10)
-utils.save_loss(hist)
+if args.debug:
+    print('* Saving loss plot to {}'.format(out_dir))
+    utils.save_loss(hist)
 
 # EVALUATE MODEL
 print('* Evaluating model..')
-test_data = SymbolManager(test_symbols, features, timestep, futurestep, debug=debug)
+test_data = SymbolManager(test_symbols, features, timestep, futurestep, debug=args.debug)
 print('** {} ↓time windows - {} ↑time windows'.format(utils.count_neg(test_data.y), utils.count_pos(test_data.y)))
 
 test_results = model.evaluate(test_data.x, test_data.y)
 print('** test loss: {} - test accuracy: {}'.format(test_results[0], test_results[1]))
 
-print('* Saving results to /out for {} test symbols'.format(len(test_symbols)))
+print('* Saving results to {} for {} test symbols'.format(out_dir, len(test_symbols)))
 preds = model.predict_classes(test_data.x)
 utils.save_predictions(test_data, preds)
