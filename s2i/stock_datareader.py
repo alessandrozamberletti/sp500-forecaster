547ecc194ff0b08684fd9e65d60962907429b657
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-09-07 14:34:29 +0200

    * added examples * wip

--- s2i/stock_datareader.py
+++ s2i/stock_datareader.py
@@ -4,34 +4,40 @@ pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader as web
 
 
-def get_ohlcv(stocks):
+def get_ohlcv(symbols):
     """
-    Returns a key-value pair dictionary of OHLCV data for the given stocks (data is read from IEX).
+    Retrieve OHLCV data for the given symbols (data read from IEX).
 
-    :param stocks: array
-        The stocks for which to retrieve OHLCV information.
+    Args:
+        symbols (str array): The symbols for which to retrieve OHLCV information.
 
-    :return: Dictionary
+    Returns:
+        dict: Key-value pair dictionary of OHLCV data.
+
+    Examples:
+        >>> [(k,v.size) for k, v in stock_datareader.get_ohlcv(['AAPL', 'MSFT']).iteritems()]
+        [('AAPL', 6295), ('MSFT', 6295)]
+        >>> [(k,v.columns.tolist()) for k, v in stock_datareader.get_ohlcv(['NFLX']).iteritems()]
+        [('NFLX', [u'open', u'high', u'low', u'close', u'volume'])]
     """
-    stock_data = {}
 
     # cannot retrieve data for symbols unsupported by IEX
-    stocks = __drop_unsupported_symbols(stocks)
-    assert len(stocks) > 0, 'none of the given symbols are supported by IEX stock exchange'
+    symbols = __drop_unsupported_symbols(symbols)
+    assert len(symbols) > 0, 'none of the given symbols are supported by IEX stock exchange'
 
     # see: https://pandas-datareader.readthedocs.io/en/latest/remote_data.html#remote-data-iex
     start = datetime.now() - timedelta(days=2000)
-
-    for stock in stocks:
+    stock_data = {}
+    for symbol in symbols:
         # NOTE: extracting too many symbols at once causes key error in IEXDailyReader
         # noinspection PyBroadException
         try:
-            ohlcv = web.DataReader(stock, data_source='iex', start=start)
+            ohlcv = web.DataReader(symbol, data_source='iex', start=start)
         except Exception:
-            print('no data for SYMBOL:{}, skipping'.format(stock))
+            print('no data for SYMBOL:{}, skipping'.format(symbol))
             continue
 
-        stock_data[stock] = ohlcv
+        stock_data[symbol] = ohlcv
 
     return stock_data
 
