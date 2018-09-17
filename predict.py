dff4da9dd665b342c7ddedda15895278373b6a22
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-09-17 17:36:57 +0200

    * initial commit

--- predict.py
+++ predict.py
@@ -3,3 +3,21 @@
 # ** model path
 # output:
 # ** [predict_future(symbol) for symbol in symbols]
+from sp500forecaster.args import parse_predict_args
+from sp500forecaster import log, set_console_logger
+import os
+
+
+def predict(args):
+    pass
+
+
+if __name__ == '__main__':
+    args = parse_predict_args()
+
+    if args.verbose:
+        set_console_logger()
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+
+    predict(args)
--- sp500forecaster/args.py
+++ sp500forecaster/args.py
@@ -5,8 +5,20 @@ def parse_create_args():
     parser = ArgumentParser(description='Create a model to predict future stock trends of SP500 companies.')
     parser.add_argument('stocknum', type=int, help='number of sp500 stocks to retrieve (0=all)')
     parser.add_argument('epochs', type=int, help='number of training epochs')
+    parser.add_argument('-d', '--debug', action='store_true', default=False, help='visual train info, default false')
+    parser = _add_common(parser)
+    return parser.parse_args()
+
+
+def parse_predict_args():
+    parser = ArgumentParser(description='Predict future stock trends using a pretrained forecaster.')
+    parser.add_argument('model', type=str, help='path to h5 forecaster model file')
+    parser.add_argument('symbols', nargs='+', help='list of 1 or more iex-supported tickers')
+    parser = _add_common(parser)
+    return parser.parse_args()
+
+
+def _add_common(parser):
     parser.add_argument('-o', '--output', default='out', help='path to output directory (default ./out)')
     parser.add_argument('-v', '--verbose', action='store_true', default=True, help='verbose output, default true')
-    parser.add_argument('-d', '--debug', action='store_true', default=False, help='visual train info, default false')
-
-    return parser.parse_args()
+    return parser
